267233eca47d6416c1a93b3f4628d32883b0d1ec
rafael andrade <rafanidius220@gmail.com>
2022-11-29 23:16:45 -0300

    mais um pouco

--- compelado/VM.c
+++ compelado/VM.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-char m[50];//memoria
+char Memoria[50];//memoria
 int l;//linha
 int p;//flag
 FILE *arquivo;
@@ -15,7 +15,7 @@ int pilha()
     int j = 50;
 	for(j=0;j>=0;j--)
     {
-		printf("%d\n",m[j]);
+		printf("%d\n",Memoria[j]);
 	}
 	return 0;
 }
@@ -42,4 +42,46 @@ char charATUAL;
 		charATUAL = fgetc(arquivo);
 	}
 	return r;
+}
+int execucao()
+{
+	char rotulo [10];
+	char comando [10];
+	char param1 [10];
+	char param2 [10];
+
+	if(!strcmp(comando,"START   "))
+	{
+		printf("start\n");
+		p=-1;
+		l++;
+	}//start
+		else{	
+			if(!strcmp(comando,"ALLOC   "))
+			{
+				int m,n,k;
+				m=atoi(param1);
+				n=atoi(param2);
+				printf("alloc %d %d\n",m,n);//alocacao da memoria
+				for(k=0;k<n;k++){
+					p++;
+					Memoria[p]=Memoria[m+k];
+				}
+				l++;
+			}else{
+
+			if(!strcmp(comando,"DALLOC  "))
+			{
+				int m,n,k;
+				m=atoi(param1);
+				n=atoi(param2);
+				printf("dalloc %d %d\n",m,n);//dealocacao
+				for(k=n-1;k>=0;k--){
+					Memoria[m+k]= Memoria[p];
+					p--;
+				}
+				l++;
+			}
+		}
+	}
 }
